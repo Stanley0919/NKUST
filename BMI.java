b0a0949126d3618ad973c3fc78e9b4f6cad91a46
Stantey0919 <a0977216739@gmail.com>
2021-11-10 23:56:34 +0800

    Add BMI file

---
+++ BMI.java
@@ -0,0 +1,12 @@
+public class BMI {
+    public static void main(String[] args) {
+
+        int weight = 65;
+        float height = 1.75F;
+        float BMI = weight/(height*height);
+        System.out.println(BMI);
+
+
+    }
+}
+
